4f501df13dc1b4b287ae15c4e695a3ba6d752b83
sunforce <mwieneke@gmail.com>
2014-11-01 14:45:48 -0400

    First python script

---
+++ script1.py
@@ -0,0 +1,9 @@
+# A first Python script
+import sys
+print(sys.platform)
+print(2 ** 100)
+x = 'Spam!'
+print (x * 8)
+y=input('Type anything you want printed out')
+print (y)
+input('Press Enter to Exit')
