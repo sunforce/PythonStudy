1ee7278e1c268a8186be93462fe87966b25f58bd
Myron Wieneke <mwieneke@gmail.com>
2015-06-13 21:08:36 -0400

    merging old and new study

---
+++ Chap2/script1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys			# Load a library module
+print(sys.platform)
+print(2 ** 100)		# raise 2 to a power
+x = 'Spam!'
+print(x * 8)		# String repetition
+y = input('Press enter to exit')
---
+++ Chap3/script1.py
@@ -0,0 +1,7 @@
+#!/Library/Frameworks/Python.framework/Versions/3.4/bin/python3
+# A first Python script
+import sys		# Load a library module
+print(sys.platform)
+print(2 ** 100)	# Raise 2 to a power
+x = 'Spam!'
+print(x * 32)	# String repetition
