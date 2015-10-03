3d02106ed0b448c61bdcdf312f069fc6b0c07a68
Myron <sunforce@Myrons-iMac.local>
2015-10-03 18:58:00 -0400

    modifying battleship.py for multiplayer and OO, added testing.py

--- battleship.py
+++ battleship.py
@@ -1,31 +1,60 @@
+#!/usr/bin/python
 from random import randint
 
+class battleboard(object):
+
+    def __init__(self, name, board_size, board, num_ships):
+        self.name = name
+        self.board_size = board_size
+        self.board = board
+        self.num_ships = num_ships
+
+    def create_board(self):
+        for x in range(self.board_size):
+            self.board.append(["O"] * board_size)
+
+    def print_board(self):
+        print self.name + "'s Board"
+        for row in self.board:
+            print " ".join(row)
+
+    def random_row(self):
+        for ships in range(self.num_ships):
+            shipsrandint(0, len(self.board) - 1)
+
+    def random_col(self):
+        return randint(0, len(self.board) - 1)
+
+# Initialize the game
+board_size = 5
+num_turns = 5
 board = []
+firstplayer_name = raw_input("First Players name? ")
+secondplayer_name = raw_input("Second Players name? ")
+firstplayer = battleboard(firstplayer_name, board_size, board)
+secondplayer = battleboard(secondplayer_name, board_size, board)
+firstplayer.create_board()
+secondplayer.create_board()
+first_ship_row = firstplayer.random_row()
+first_ship_col = firstplayer.random_col()
+second_ship_row = secondplayer.random_row()
+second_ship_col = secondplayer.random_col()
 
-for x in range(5):
-    board.append(["O"] * 5)
+print "First Player Ship location"
+print first_ship_row
+print first_ship_col
 
-def print_board(board):
-    for row in board:
-        print " ".join(row)
+print "Second Player Ship location"
+print second_ship_row
+print second_ship_col
 
 print "Let's play Battleship!"
-print_board(board)
 
-def random_row(board):
-    return randint(0, len(board) - 1)
+firstplayer.print_board()
+secondplayer.print_board()
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
-
-ship_row = random_row(board)
-ship_col = random_col(board)
-print ship_row
-print ship_col
-
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
-for turn in range(4):
+# Lets run the game
+for turn in range(num_turns):
     # Print (turn + 1) here!
     print "Turn", turn + 1
     # Make a guess
