8afa9cd2dc790deafb2bea109783988086199dbe
Myron <sunforce@Myrons-iMac.local>
2015-10-23 00:01:04 -0400

    updated battleship file

---
+++ testing.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+print len(board)
+print len(board[0])
