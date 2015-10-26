11d15b7c493c6adfadae87cb2b90cf0b903e6c74
Myron <sunforce@Myrons-iMac.local>
2015-10-26 07:27:16 -0400

    finished

--- battleship.py
+++ battleship.py
@@ -6,7 +6,6 @@ class BattleBoard(object):
     def __init__(self, name, board_size, num_ships):
         self.player_ships = defaultdict(list)
         self.board = []
-        self.coords = []
         self.name = name
         self.board_size = int(board_size)
         self.num_ships = int(num_ships)
@@ -18,15 +17,26 @@ class BattleBoard(object):
         for row in self.board:
           print " ".join(row)
     def random_ships(self):
-        for ships in range(self.num_ships):
-            x_coord = randint(0, len(self.board) - 1)
-            y_coord = randint(0, len(self.board) - 1)
-            self.player_ships[ships].append([x_coord, y_coord])
+        flag = False
+        ship = 0
+        temp_coords = []
+        while ship < self.num_ships:
+            temp_coords = randint(1, len(self.board)), randint(1, len(self.board))
+            # Make sure coordinates are unique
+            for scan_ship in range(self.num_ships):
+                if self.player_ships[scan_ship] == [temp_coords]:
+                    flag = True  
+            if flag == False:
+                self.player_ships[ship].append(temp_coords)
+                ship = ship + 1
+            flag = False
+
 
 # Initialize the game
-board_size = 5
-num_turns = 5
-num_ships = raw_input("Number of ships in the game?")
+num_ships = int(raw_input("Number of ships in the game?"))
+board_size = 5 + num_ships - 1
+num_turns = num_ships * 3
+player_turn = 0
 firstplayer_name = raw_input("First Players name? ")
 secondplayer_name = raw_input("Second Players name? ")
 firstplayer = BattleBoard(firstplayer_name, board_size, num_ships)
@@ -35,39 +45,54 @@ firstplayer.random_ships()
 secondplayer = BattleBoard(secondplayer_name, board_size, num_ships)
 secondplayer.create_board()
 secondplayer.random_ships()
-
+# Cheat
 print "First Player Ship location"
 print firstplayer.player_ships
-
 print "Second Player Ship location"
 print secondplayer.player_ships
 
 print "Let's play Battleship!"
-
 firstplayer.print_board()
 secondplayer.print_board()
-
 # Lets run the game
-for turn in range(num_turns):
-    # Print (turn + 1) here!
-    print "Turn", turn + 1
+for turn in range(num_turns * 2):
+    # switch turns
+    if player_turn == 0:
+        print ("Player %s Turn %s" % (firstplayer_name, turn + 1))
+        currentplayer = secondplayer
+        player_turn = 1
+    else:
+        print ("Player %s Turn %s" % (secondplayer_name, turn + 1))
+        currentplayer = firstplayer
+        player_turn = 0
+    turn_ship = 0
+    turn_hit = False
     # Make a guess
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
     
     #Evaluate guess
-    if guess_row == ship_row and guess_col == ship_col:
-        print "Congratulations! You sunk my battleship!"
-        break
+    if (guess_row < 1 or guess_row > board_size) or (guess_col < 1 or guess_col > board_size):
+        print "Oops, that's not even in the ocean."
+    elif(currentplayer.board[guess_row-1][guess_col-1] == "X"):
+        print "You guessed that one already."
+    elif(currentplayer.board[guess_row-1][guess_col-1] == "S"):
+        print "You sank that ship already."
     else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print "Oops, that's not even in the ocean."
-        elif(board[guess_row-1][guess_col-1] == "X"):
-            print "You guessed that one already."
-        else:
+        turn_coords = [(guess_row, guess_col)]
+        for scan_hit in range(num_ships):
+            if cmp(currentplayer.player_ships[scan_hit], turn_coords) == 0:
+                currentplayer.board[guess_row-1][guess_col-1] = "S"
+                currentplayer.num_ships=currentplayer.num_ships-1
+                print "Congratulations! You sunk a battleship!" 
+                turn_hit = True
+        if turn_hit == False:
             print "You missed my battleship!"
-            board[guess_row-1][guess_col-1] = "X"
-    print_board(board)
-    if turn == 3:
-        print "Game Over"    
+            currentplayer.board[guess_row-1][guess_col-1] = "X"
+    currentplayer.print_board()
+    print "%s has %s out of %s battleships left" % (currentplayer.name, currentplayer.num_ships, num_ships)
+    if currentplayer.num_ships == 0:
+        print "%s has lost!" % (currentplayer.name)
+        break
+print "Game Over"
     
